c6dcee846a41f9e861944ba47c2523e843497c41
Kristina <cyberkitty628@gmail.com>
2020-10-03 00:17:19 -0700

    Added next function

--- poetryslam.py
+++ poetryslam.py
@@ -6,3 +6,12 @@ def get_file_lines(filename):
     infile = open(filename, "r")
     read_lines = infile.readlines()
     return read_lines
+
+# Assigned prev function as new lines_list variable to retrieve poem list within next function
+
+lines_list = get_file_lines(filename)
+
+# Used for loop in order for function to print reverse enumerated version of poem list
+def lines_printed_backwards(lines_list):
+    for line, value in reversed(list(enumerate(lines_list))):
+        print(line, value)
