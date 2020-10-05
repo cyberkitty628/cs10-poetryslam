3ca6f48e326cb0b564b5078966a18363a542d302
Kristina <cyberkitty628@gmail.com>
2020-10-04 22:43:52 -0700

    Added further comments

--- poetryslam.py
+++ poetryslam.py
@@ -33,6 +33,9 @@ def lines_printed_random(lines_list):
 
 lines_printed_random(lines_list)
 
+# For the last step, I'm printing the poem with a heart symbol in front of each line starting from the top
+# I'm also separating the hearts with a "~" symbol, for further aesthetic purposes
+# Removed spaces between lines to match previous step outputs
 def lines_printed_custom(lines_list):
       for line in lines_list:
           print("♥", line, sep=" ~ ", end=" ")
